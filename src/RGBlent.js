22b95e8d276f3416d0cc879ce834ef411fe93e03
Val Currie <contact@va.lcuri.email>
2021-09-16 11:42:36 -0500

    added separate state for main loaded color and a button to sync with the picker

--- src/RGBlent.js
+++ src/RGBlent.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { HexColorPicker } from "react-colorful";
-import { Container, Col, Row } from "@bootstrap-styled/v4";
+import { Container, Col, Row, Button } from "@bootstrap-styled/v4";
 import { Palette } from "./components/Palette.js";
 import { Swatch } from "./components/Swatch.js";
 
 export const RGBlent = (props) => {
   const [pickerColor, setPickerColor] = useState("#80ff80");
+  const [color, setColor] = useState("#80ff80");
 
   return (
     <Container>
       <Row>
         <Col>
-          <HexColorPicker
-            style={{ margin: "auto", marginTop: "6rem" }}
-            color={pickerColor}
-            onChange={setPickerColor}
-          />
-          <Swatch style={{ marginTop: "4rem" }} color={pickerColor} size={12} />
+          <Row
+            // TODO set up proper breakpoints so this spacing works on mobile
+            style={{ margin: "auto", marginLeft: "4rem", marginTop: "6rem" }}
+          >
+            <Col style={{ margin: "auto" }}>
+              <HexColorPicker
+                style={{ margin: "auto", marginLeft: "8rem" }}
+                color={pickerColor}
+                onChange={setPickerColor}
+              />
+            </Col>
+            <Col style={{ margin: "0" }}>
+              <Button onClick={() => setColor(pickerColor)}>Load Color</Button>
+            </Col>
+          </Row>
+          <Swatch style={{ marginTop: "4rem" }} color={color} size={12} />
           <Palette />
         </Col>
         <RightColumn>
