e6a03a293504ed4d1b02dcf7d059bec1e66eae6c
Val Currie <contact@va.lcuri.email>
2021-09-15 23:22:11 -0500

    finished swatch component and left one in RGBlent where the loaded color will go

--- src/RGBlent.js
+++ src/RGBlent.js
@@ -2,47 +2,30 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { HexColorPicker } from "react-colorful";
 import { Swatch } from "./components/Swatch.js";
+import { Container, Col, Row } from "@bootstrap-styled/v4";
 
 export const RGBlent = (props) => {
   const [pickerColor, setPickerColor] = useState("#80ff80");
 
   return (
-    <ColumnContainer>
-      <LeftColumn>
-        <Separator height={1} />
-        <HexColorPicker color={pickerColor} onChange={setPickerColor} />
-        <Separator height={1} />
-        <Swatch color={pickerColor} />
-        <Separator height={1} />
-      </LeftColumn>
-      <RightColumn>
-        <MockSidebar />
-      </RightColumn>
-    </ColumnContainer>
+    <Container>
+      <Row>
+        <Col>
+          <HexColorPicker
+            style={{ margin: "auto", marginTop: "2rem" }}
+            color={pickerColor}
+            onChange={setPickerColor}
+          />
+          <Swatch style={{ marginTop: "4rem" }} color={pickerColor} size={12} />
+        </Col>
+        <RightColumn>
+          <MockSidebar />
+        </RightColumn>
+      </Row>
+    </Container>
   );
 };
 
-const width = (props) => props.width;
-const height = (props) => props.height;
-
-const Separator = styled.div`
-  width: ${width}rem;
-  height: ${height}rem;
-`;
-
-const ColumnContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  border: 1px solid black;
-`;
-
-const LeftColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px dotted black;
-  align-items: center;
-`;
 const RightColumn = styled.div`
   display: flex;
   flex-direction: column;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BootstrapProvider from "@bootstrap-styled/provider/lib/BootstrapProvider";
 import { RGBlent } from "./RGBlent.js";
+
+const theme = {
+  "$body-bg": "lightgrey",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <RGBlent />
+    <BootstrapProvider theme={theme}>
+      <RGBlent />
+    </BootstrapProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
