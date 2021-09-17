6b1f2265e59666fe9dc95ffb0f9e835d2b1cd965
Val Currie <contact@va.lcuri.email>
2021-09-17 16:58:09 -0500

    installed and set up react-query

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import BootstrapProvider from "@bootstrap-styled/provider/lib/BootstrapProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RGBlent } from "./RGBlent.js";
 
 const theme = {
   "$body-bg": "lightgrey",
 };
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BootstrapProvider theme={theme}>
-      <RGBlent />
-    </BootstrapProvider>
+    <QueryClientProvider client={queryClient}>
+      <BootstrapProvider theme={theme}>
+        <RGBlent />
+      </BootstrapProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
