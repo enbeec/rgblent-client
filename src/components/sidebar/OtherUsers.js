622a5f57ccd927bd2750f8be5d2946ad3feabf8f
Val Currie <contact@va.lcuri.email>
2021-10-07 15:34:03 -0500

    other users have mini palettes

--- src/components/sidebar/OtherUsers.js
+++ src/components/sidebar/OtherUsers.js
@@ -5,8 +5,35 @@ import {
   Accordion,
   AccordionGroup,
 } from "@bootstrap-styled/v4";
-import { MiniPalette } from "./MiniPalette.js"
+import { useQuery } from "react-query";
+import { MiniPalette } from "./MiniPalette.js";
+import { AuthContext } from "../auth/AuthProvider.js";
+import { authFetch } from "../../utils/fetch.js";
 
 export const OtherUsers = (props) => {
-	return null
-}
+  const { profile } = useContext(AuthContext);
+  const users = useQuery(
+    "users",
+    () => authFetch("/users").then((res) => res.json()),
+    { noAuth: true }
+  );
+
+  return (
+    users.isFetching || (
+      <Card>
+        {users.data.map((u) => {
+          if (!u.colors.length || !u.palettes.length) return null;
+          if (u.id === profile?.data.id) return null;
+          return (
+            <Card>
+              <CardHeader>{u.name}</CardHeader>
+              {u.palettes.map((p) => (
+                <MiniPalette palette={p} />
+              ))}
+            </Card>
+          );
+        })}
+      </Card>
+    )
+  );
+};
--- src/components/sidebar/Profile.js
+++ src/components/sidebar/Profile.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react"
-import { Card} from "@bootstrap-styled/v4"
+import { Card } from "@bootstrap-styled/v4"
 import { AuthContext } from "../auth/AuthProvider.js"
 import { MiniPalette } from "./MiniPalette.js"
 
 export const Profile = ({...props}) => {
 	const { profile } = useContext(AuthContext)	
-	return profile.isLoading || <>
+	return (profile.isLoading || !profile.data) || <Card>
 		{profile.data.palettes.map((p) => (<MiniPalette palette={p} />))}
-	</>
+	</Card>
 }
